b1a9dc124267b1e3b7706b91255a50c599b8c6b3
Qadir Bux <167604642+Qadir09@users.noreply.github.com>
2026-02-02 08:49:16 -0800

    Path file of splash screen
    Add splashscreen path file in main.dart

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Splashscreen extends StatefulWidget{
+class Splashscreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     throw UnimplementedError();
   }
-  Scaffold build(BuildContext context){
-    return Scaffold(
-      
-    )
-  }
 
+  Scaffold build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white, body: Center());
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hasib_app/SplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(), // Start with splash screen
+      home: Splashscreen(), // Start with splash screen
     );
   }
 }
