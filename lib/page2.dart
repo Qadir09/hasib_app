e483c26b8f8a791035ff014988ac6eedf01dbea5
Qadir Bux <167604642+Qadir09@users.noreply.github.com>
2026-02-28 07:34:56 -0800

    Update page2.dart
    add Appbar of page2.dart for Analytics

--- lib/page2.dart
+++ lib/page2.dart
@@ -5,6 +5,22 @@ class AnalyticsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(body: Center(child: Text('secod')));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "Analytics",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.calendar_today, color: Colors.black),
+          ),
+        ],
+      ),
+    );
   }
 }
